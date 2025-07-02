33413f1c8ef694f4288434f6649d5305d89f0469
Sindhu-182006 <tsindhuthippavaramsindhu@gmail.com>
2025-07-02 18:26:55 +0530

    Add files via upload

---
+++ funexp.js
@@ -0,0 +1,16 @@
+function findoddoreven(no){
+    if(no%2==0){
+        console.log("even");
+    }else{
+        console.log("odd");
+    }
+}
+const oddoreven=(no)=>{
+    if(no%2==0){
+         console.log("even");
+    }else{
+        console.log("odd");
+    }
+}
+findoddoreven(17);
+oddoreven
